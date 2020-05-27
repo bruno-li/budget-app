7d71c98cc2990f051c5037422d7365aa2aaf108d
Bruno Li <lisiuloong@gmail.com>
2020-05-26 23:03:09 -0400

    add public methods to the ui-controller

--- src/app.js
+++ src/app.js
@@ -4,20 +4,21 @@ import './vendor';
 import './style.scss';
 
 const controler = (function(budgetCtrl, UICtrl) {
+	let DOM = UICtrl.getDOMstrings();
 	// get filed input data
 	let desc = document.querySelector('.add__description');
 	let value = document.querySelector('.add__value');
-	let addBtn = document.querySelector('.add__btn');
+	let addBtn = document.querySelector(DOM.addValue);
 
 	const addItem = () => {
-		// add the item to the budget controler
-		// add the item to the UI
-		// calculate budget
-		// display the budget
+		// get input fields data
+		const input = UICtrl.getInput();
+		console.log(input);
 	};
 
 	addBtn.addEventListener('click', addItem);
 
+	// listen for return press
 	document.addEventListener('keypress', (e) => {
 		if (e.keyCode === 13) {
 			addItem();
--- src/ui-controller.js
+++ src/ui-controller.js
@@ -1,1 +1,22 @@
-const UIController = (function() {})();
+export const UIController = (function() {
+	// store all the DOM element for easy change
+	let DOMstrings = {
+		inputType: '.add__type',
+		inputDescription: '.add__description',
+		inputAddValue: '.add__value',
+		addValue: '.add__btn'
+	};
+	return {
+		getInput: () => {
+			return {
+				typeOfBudget: document.querySelector(DOMstrings.inputType).value,
+				budgetDescription: document.querySelector(DOMstrings.inputDescription).value,
+				budgetValue: document.querySelector(DOMstrings.inputAddValue).value
+			};
+		},
+
+		getDOMstrings: () => {
+			return DOMstrings;
+		}
+	};
+})();
